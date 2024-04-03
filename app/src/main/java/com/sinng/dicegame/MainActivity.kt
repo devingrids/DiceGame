e695d92022a0a1818f69eb78fbb8e22c4fb451e9
Ingrid Silva <sinngtec@gmail.com>
2024-04-03 20:07:46 -0300

    add first composable

--- app/src/main/java/com/sinng/dicegame/MainActivity.kt
+++ app/src/main/java/com/sinng/dicegame/MainActivity.kt
@@ -3,13 +3,27 @@ package com.sinng.dicegame
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.sinng.dicegame.ui.theme.DiceGameTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +36,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    App()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun App() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        Canvas(
+            modifier = Modifier
+                .size(96.dp, 96.dp)
+                .align(Alignment.Center)
+        ) {
+            drawRoundRect(
+                Color.Green,
+                cornerRadius = CornerRadius(20f, 20f),
+                topLeft = Offset(10f, 10f),
+                size = size
+            )
+
+            drawCircle(
+                Color.Black,
+                radius = Dp(20f).value,
+                center = Offset(size.width / 2, size.width / 2)
+            )
+
+        }
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset(y = (100).dp)
+        ) {
+            Text(text = "Jogar")
+
+
         }
     }
 }
@@ -40,7 +93,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    DiceGameTheme {
-        Greeting("Android")
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        App()
     }
 }
